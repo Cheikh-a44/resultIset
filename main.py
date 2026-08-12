5a0b54c401aa9eca35d484e988a5a60e23fb3f58
Ch-a44 <cheikhahmedoumedlemine@gmail.com>
2026-08-12 06:19:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -407,6 +407,22 @@ class BeloteRoot(BoxLayout):
         scroll.add_widget(self.history_layout)
 
         self.add_widget(scroll)
+# app info 
+        app_info = Label(
+    text="By Cheikh_A\nVersion 1.0",
+    font_size="11sp",
+    color=(1, 1, 1, 0.45),
+    halign="center",
+    valign="middle",
+    size_hint=(1, None),
+    height=dp(38)
+)
+
+app_info.bind(
+    size=app_info.setter("text_size")
+)
+
+self.add_widget(app_info)
 
     # =====================================================
     # Create Input
